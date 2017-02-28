fa38a87049a31aa34fa4e73a9593612ac76e6718
Peter Wunder <hallo@peterwunder.de>
2017-02-28 10:57:46 +0100

    Sharkle is now a presentable prototype. He animates and shows the "SHARKLE" bubbles when clicking on him.

--- Sharkle/MainWindow.xaml.cs
+++ Sharkle/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +22,18 @@ namespace Sharkle {
 	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
+
+			Timeline.DesiredFrameRateProperty.OverrideMetadata(typeof(Timeline), new FrameworkPropertyMetadata { DefaultValue = 12 });
+
+			//this.Resources["SharkleBGBrush"] = new SolidColorBrush(Color.FromRgb(255, 127, 0));
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+			DragMove();
 		}
 	}
 }
