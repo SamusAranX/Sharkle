8ff518383efc8973a573c69e99355540a6592296
Peter Wunder <hallo@peterwunder.de>
2017-03-07 01:08:42 +0100

    Bump to v1.0. IT LIVES!
    Redid pretty much every single image, making them easier to handle
    internally
    Moved the Storyboard animation stuff in the main window from XAML to
    code
    Revisited all Storyboards and cleaned up unused/unwanted stuff
    Added a taskbar icon and an About window featuring a bunch of links
    holy shit i cant believe this took me a whole week

---
+++ Sharkle/AboutWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sharkle {
+	/// <summary>
+	/// Interaction logic for AboutWindow.xaml
+	/// </summary>
+	public partial class AboutWindow : Window {
+		public AboutWindow() {
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		public string CurrentVersion {
+			get {
+				var version = ApplicationDeployment.IsNetworkDeployed
+					   ? ApplicationDeployment.CurrentDeployment.CurrentVersion
+					   : Assembly.GetExecutingAssembly().GetName().Version;
+				
+				return version.ToString(3);
+			}
+		}
+
+		private Array _hyperlinks = new[] {
+			new Uri("http://www.nightinthewoods.com/"),
+			new Uri("https://www.youtube.com/watch?v=d86WnX_271U&feature=youtu.be&t=1h34m30s"),
+			new Uri("https://github.com/SamusAranX/Sharkle/"),
+			new Uri("https://github.com/SamusAranX/Sharkle/issues"),
+			new Uri("https://twitter.com/SamusAranX"),
+			new Uri("https://peterwunder.de"),
+		};
+		public Array Hyperlinks {
+			get {
+				return _hyperlinks;
+			}
+		}
+
+		public Color WindowGlassColor {
+			get {
+				return SystemParameters.WindowGlassColor;
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+		}
+
+		private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) {
+			var link = (Hyperlink)e.OriginalSource;
+			Process.Start(link.NavigateUri.AbsoluteUri);
+		}
+	}
+}
--- Sharkle/MainWindow.xaml.cs
+++ Sharkle/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace Sharkle {
 	/// <summary>
@@ -36,14 +37,31 @@ namespace Sharkle {
 
 		public MainWindow() {
 			InitializeComponent();
-
-			Timeline.DesiredFrameRateProperty.OverrideMetadata(typeof(Timeline), new FrameworkPropertyMetadata { DefaultValue = 12 });
+			
 			this._rnd = new Random();
 			this.DataContext = this;
 		}
 
-		private void Window_Loaded(object sender, RoutedEventArgs e) {
+		// These Storyboards have to be controlled in code because the WPF team has no clue what it's fucking doing
+		private Storyboard SharkleIdleAnim, SharkleWavingAnim, SharkleShowBubblesAnim, SharkleBubbleFlickerAnim;
 
+		private TaskbarIcon SharkleIcon;
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			var workingArea = SystemParameters.WorkArea;
+			var padding = 20;
+			this.Left = workingArea.Right - this.Width - padding;
+			this.Top = workingArea.Bottom - this.Height - padding;
+
+			SharkleIdleAnim = (Storyboard)FindResource("SharkleIdleAnim");
+			SharkleBubbleFlickerAnim = (Storyboard)FindResource("SharkleBubbleFlickerAnim");
+			SharkleWavingAnim = (Storyboard)FindResource("SharkleWavingAnim");
+			SharkleShowBubblesAnim = (Storyboard)FindResource("SharkleShowBubblesAnim");
+
+			SharkleIcon = (TaskbarIcon)FindResource("SharkleIcon");
+
+			// I'm starting these two Storyboards manually here because otherwise, the .Seek()s below won't work
+			SharkleIdleAnim.Begin();
+			SharkleBubbleFlickerAnim.Begin();
 		}
 
 		bool isPlaying = false;
@@ -51,6 +69,15 @@ namespace Sharkle {
 			DragMove();
 
 			if (!isPlaying) {
+				// Manually rewind that storyboard to the beginning because I am sick of WPF's bullshit
+				SharkleBubbleFlickerAnim.Seek(TimeSpan.Zero);
+
+				// Start waving
+				SharkleWavingAnim.Begin();
+				SharkleShowBubblesAnim.Begin();
+
+				// Play a sound and wait for it to end
+				// When isPlaying == true, clicking on Sharkle will do nothing
 				isPlaying = true;
 				await Task.Run(() => {
 					var sndResource = App.GetResourceStream(new Uri(this.HeySource));
@@ -58,8 +85,32 @@ namespace Sharkle {
 					sndPlayer.PlaySync();
 				});
 				isPlaying = false;
+
+				// Fetch a new sound bite
 				OnPropertyChanged("HeySource");
 			}
+		}
+
+		private void SharkleShowBubblesAnim_Completed(object sender, EventArgs e) {
+			// The waving animation has finished and the idle animation is about to be displayed again
+			// Rewind idle animation to the beginning to avoid the transition looking weird
+			SharkleIdleAnim.Seek(TimeSpan.Zero);
+		}
+
+		private void MenuItem_About_Click(object sender, RoutedEventArgs e) {
+			foreach (var window in App.Current.Windows) {
+				if (window is AboutWindow) {
+					((AboutWindow)window).Activate();
+					return;
+				}
+			}
+
+			var aboutWindow = new AboutWindow();
+			aboutWindow.Show();
+		}
+
+		private void MenuItem_Exit_Click(object sender, RoutedEventArgs e) {
+			App.Current.Shutdown();
 		}
 
 		private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e) {
@@ -67,7 +118,7 @@ namespace Sharkle {
 		}
 		
 		//
-		// INotifyPropertyChanged
+		// INotifyPropertyChanged stuff
 		//
 
 		public event PropertyChangedEventHandler PropertyChanged;
